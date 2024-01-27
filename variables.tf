176eebd413a82da453f392303e1da31ae36b8b80
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-27 07:58:51 +0530

    TF Code

--- variables.tf
+++ variables.tf
@@ -6,6 +6,7 @@ variable "parameters" {
   default = {
     "dev.expense.rds.username" = "admin1"
     "dev.expense.rds.endpoint" = "dev-expense-rds.cbvsbeoyxek4.us-east-1.rds.amazonaws.com"
+    "dev.expense.frontend.backend_endpoint" = "https://backend-dev.rdevopsb73.online/"
   }
 }
 

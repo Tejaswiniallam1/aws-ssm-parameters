7fecaa671e98966220eed5c6fde4ab49da1fe3c8
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-12 08:05:06 +0530

    Cart Changes

--- variables.tf
+++ variables.tf
@@ -21,7 +21,10 @@ variable "parameters" {
     "dev.roboshop.docdb.endpoint"            = "dev-roboshop-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com"
     "dev.roboshop.catalogue.MONGO_URL"       = "mongodb://admin1:RoboShop12345@dev-roboshop-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
     "dev.roboshop.user.MONGO_URL"       = "mongodb://admin1:RoboShop12345@dev-roboshop-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
-    "dev.robosdhop.user.REDIS_HOST" = "dev-roboshop-elasticache.nud0cy.0001.use1.cache.amazonaws.com"
+    "dev.roboshop.user.REDIS_HOST" = "dev-roboshop-elasticache.nud0cy.0001.use1.cache.amazonaws.com"
+    "dev.roboshop.cart.REDIS_HOST" = "dev-roboshop-elasticache.nud0cy.0001.use1.cache.amazonaws.com"
+    "dev.roboshop.cart.CATALOGUE_HOST" = "catalogue"
+    "dev.roboshop.cart.CATALOGUE_PORT" = "80"
 
   }
 }
